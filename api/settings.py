b779879708cb0cec6b0b80eaad4cfd621505285e
arseni2 <47722945+arseni2@users.noreply.github.com>
2021-04-14 21:00:51 +0300

    Add files via upload

--- api/settings.py
+++ api/settings.py
@@ -18,8 +18,8 @@ DEBUG = False
 AUTH_USER_MODEL = 'apiapp.User'
 ALLOWED_HOSTS = ['herokuapp.com', 'react-movie-django']
 ACCOUNT_EMAIL_REQUIRED = False
-import django_heroku
-django_heroku.settings(locals())
+import django_on_heroku
+django_on_heroku.settings(locals())
 # Application definition
 APPEND_SLASH=False
 INSTALLED_APPS = [
